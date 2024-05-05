c28db00457f0abc8a56f0b1f5b9c4c260d13fd40
VBGitH <vb@pcs.ru>
2024-05-05 10:22:11 +0300

    init

---
+++ public/client.js
@@ -0,0 +1,79 @@
+
+let btnLoad = document.getElementById('load')
+btnLoad.onclick = () => {
+    async function success(position) {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+
+        //let res = await fetch(`http://localhost:3000/api/weather?lat=${lat}&lon=${lon}&temp=C`)
+        
+        let res = await fetch(`/api/weather?lat=${lat}&lon=${lon}&temp=C`)
+       
+        let weatherData = await res.json()
+        
+        let hAdress = document.getElementById('adress')
+        let hTemp = document.getElementById('temp')
+        let hTempC = document.getElementById('tempC')
+        let dIcon = document.getElementById('icon')
+        let dFeel = document.getElementById('feel')
+        let dHum = document.getElementById('hum')
+        let dWind = document.getElementById('wind')
+
+        hAdress.innerText = weatherData.city
+        hTemp.innerText = weatherData.temp
+        hTempC.innerText = weatherData.tempUnit
+        let imgAdr = weatherData.icon
+        dIcon.innerHTML = `<img src="${imgAdr}" alt="">`
+        dHum.innerText = "влажность: " + weatherData.humidity + "%"
+        dWind.innerText = "ветер: " + weatherData.wind + " м/с"
+
+        //история запросов
+
+        res = await fetch(`http://localhost:3000/api/requests`)
+        resJson = await res.json()
+        
+        let hTbl = document.getElementById('tblRec')
+        let htmlTable = ''
+
+        /*  Шапка таблицы */
+
+        htmlTable += `<tr>
+                        <td><strong>Дата запроса</strong></td>
+                        <td><strong>Город</strong></td>
+                        <td><strong>Температура</strong></td>
+                        <td><strong>Ветер</strong></td>
+                        <td><strong>Влажность</strong></td>
+                      </tr>`                     
+
+        /*  вывод последних 5 запросов  */
+        
+        let maxRows = 5
+        let rowCount = Object.keys(resJson).length
+        let sup = rowCount
+        let inf = Math.max(1, sup - maxRows)
+
+        for (let i = sup; i > inf; i-- ){
+          let elm = resJson[i - 1] 
+          htmlTable += `<tr>
+                          <td>${elm.dateRequest}</td>
+                          <td>${elm.city}</td>
+                          <td>${elm.temp}</td>
+                          <td>${elm.wind}</td>
+                          <td>${elm.humidity}</td>
+                        </tr>`  
+        }                      
+        
+        hTbl.innerHTML = htmlTable
+      }
+    
+      function error() {
+        status.textContent = "Unable to retrieve your location";
+      }
+    
+      if (!navigator.geolocation) {
+        status.textContent = "Geolocation is not supported by your browser";
+      } else {
+        status.textContent = "Locating…";
+        navigator.geolocation.getCurrentPosition(success, error);
+      }
+  }
---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+//mongoose.connect('mongodb://127.0.0.1:27017/meteoDB')
+mongoose.connect(process.env.DB)
+const weatherRequest = mongoose.model('weatherRequest', { 
+  city          : String,
+  lat           : String,
+  lon           : String,
+  humidity      : String,
+  wind          : String,
+  temp          : String,
+  tempUnit      : String,
+  icon          : String,
+  dateRequest   : String, 
+})
+
+app.use('/', express.static('public'));
+
+app.get('/api/weather', async (req, res) => {
+    
+    /*
+    http://localhost:3000//api/weather?lat=64.614336&lon=32.242278&temp=C
+    */
+
+    let lat = req.query.lat
+    let lon = req.query.lon
+    let temp = req.query.temp
+
+    let key = '9a116ac36af7abeb1a9817c92526e136'
+    let resData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&lang=ru`)
+    let resDataJson = await resData.json()
+
+    let tempData = ''
+    let tempUnit = ''
+    let humData = resDataJson.main.humidity
+    let windData = resDataJson.wind.speed
+    let currentDate = new Date()
+
+    if (temp == 'C'){
+        tempData = tempData + Math.round(resDataJson.main.temp - 273.15)
+        tempUnit = "°C"
+    }else{
+        tempData = tempData + resDataJson.main.temp
+        tempUnit = "°K"    
+    }
+
+    let icon = "https://openweathermap.org/img/wn/" + resDataJson.weather[0].icon + "@2x.png"
+  
+    res.json({
+        'city' : resDataJson.name,
+        'lat' : resDataJson.coord.lat,
+        'lon' : resDataJson.coord.lon,
+        'humidity' : humData,
+        'wind' : windData,
+        'temp' : tempData,
+        'tempUnit' : tempUnit,
+        'icon' : icon
+    })
+
+    /* ---- запись БД ---- */
+    const wRequest = new weatherRequest()
+    wRequest.city = resDataJson.name
+    wRequest.lat = resDataJson.coord.lat
+    wRequest.lon = resDataJson.coord.lon
+    wRequest.humidity = humData
+    wRequest.wind = windData
+    wRequest.temp = tempData
+    wRequest.tempUnit = tempUnit
+    wRequest.icon = icon
+    wRequest.dateRequest = currentDate
+  
+    await wRequest.save()
+
+  })
+
+app.get('/api/requests', async (req, res) => {
+  
+    let mReq = await weatherRequest.find()
+    res.json(mReq)
+
+  })
+
+app.listen(port, () => {
+  //console.log(`Example app listening on http://localhost:${port}`)
+})
